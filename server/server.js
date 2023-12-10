52f0ede27516a47cd1bee9b3df5abdb7067370fb
Brewerjg <grammerjasonl@gmail.com>
2023-12-10 10:41:50 -0600

    spce

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 8000;
 const cors = require('cors');
 
+
 app.use(cors())
 app.use(express.json());                          
 app.use(express.urlencoded({ extended: true }));   
